e5b5a3cb17d26faae26981b63a4fc1978a7495c9
Josh <joshua.childs01@outlook.com>
2023-04-06 11:16:49 +1000

    header and amount format

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv
 
 input_dir = "C:/Users/joshu/Documents/Projects/gfa-scroller-script/"
-output_dir = "C:/Users/joshu/Documents/Projects/gfa-scroller-script/out/"
+output_dir = "//192.168.33.13/ChampionData/ScrollerControl/scrollers/"
 
 filename = input("File Name: ")
 
@@ -11,10 +11,11 @@ with open(input_dir + filename + '.csv', "r") as file:
 
     i = 0   # Row num
     j = 0   # File num
-    header = next(reader)[:2]   # Header Row
+    header = ['NAME', 'AMOUNT']   # Header Row
+    next(reader)
     for row in reader:
         row = row[:2]       # Remove last column
-
+        row[1] = "".join(row[1].split(','))
         if i == 50:
             # Output file
             scrollerName = "scroller_" + str(j) + ".csv"
