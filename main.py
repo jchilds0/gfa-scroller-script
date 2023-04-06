c4669b4287707e66e95acd7ab5ade65b62ecfc60
Josh <joshua.childs01@outlook.com>
2023-04-06 11:37:53 +1000

    update start number using output dir

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import csv
 import math
+from os import listdir
 
 input_dir = "C:/Users/joshu/Documents/Projects/gfa-scroller-script/"
 output_dir = "//192.168.33.13/ChampionData/ScrollerControl/scrollers/"
 
 filename = input("File Name: ")
 
+
+def current_num():
+    lst = [string.lstrip("scroller_").rstrip(".csv") for string in listdir(output_dir)]
+    retval = []
+
+    for string in lst:
+        try:
+            retval += [int(string)]
+        except ValueError:
+            pass
+
+    if len(retval) == 0:
+        return 1
+    else:
+        return max(retval) + 1
+
+
 with open(input_dir + filename + '.csv', "r") as file:
     reader = csv.reader(file)
     lst = []
 
     i = 0   # Row num
-    j = 1   # File num
+    j = current_num()   # File num
     header = ['NAME', 'AMOUNT']   # Header Row
     next(reader)
     for row in reader:
