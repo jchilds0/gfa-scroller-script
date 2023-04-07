78f6593c3e83efafde0aa033377d0cf90a24a9a8
Josh <joshua.childs01@outlook.com>
2023-04-07 14:47:17 +1000

    removed split excel needing to predict page name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import csv
 from os import listdir
 import pandas as pd
 
-format = "full"
+format = "split"
 inputDir = "./"
 outputDir = "./out/"
 scrollerPrefix = "scroller_"
@@ -56,24 +56,20 @@ def convert_full_excel(fname: str):
 
 
 def convert_split_excel(fname: str):
-    """ Converts an excel file with data split into sheets labelled x-x+50"""
+    """ Converts an excel file with data split into sheets"""
     df1 = pd.read_excel(inputDir + fname + ".xlsx", sheet_name=None)
     j = current_num()
+    array = list(df1.values())
 
-    for i in range(len(df1)):
-        # Format dict key (corresponds to sheet name)
-        if i == 0:
-            key = "{}-{}".format(0, 50)
-        else:
-            key = "{}-{}".format(i * 50 + 1, (i + 1) * 50)
-
+    for row in array:
         # output file name
         scrollerName = scrollerPrefix + '0' * (4 - len(str(j))) + str(j) + '.csv'
         print(scrollerName)
         with open(outputDir + scrollerName, "w", newline='') as scrollerFile:
             writer = csv.writer(scrollerFile)
             writer.writerow(header)
-            writer.writerows([[name, "$" + str(amount)] for name, amount in df1[key].values[1:][:50]])
+            writer.writerows([[name, "$" + str(amount)] for name, amount in row.values[1:52]])
+        j += 1
 
 
 filename = input("File Name: ")
